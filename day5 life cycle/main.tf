2ccf6df537aa5188d00ca8c7998fd9af81f996b4
revanth0104 <revanthgouru185@gmail.com>
2024-09-06 11:58:23 +0530

    Update main.tf

--- day5 life cycle/main.tf
+++ day5 life cycle/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test" {
     ami = "ami-02b49a24cfb95941c"
     instance_type = "t2.micro"
-    availability_zone = "ap-south-1b"
+   # availability_zone = "ap-south-1b"
     key_name = "projectkey"
     
   tags = {
